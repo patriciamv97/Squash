0fa10e36ae985e4c9f812221e95b9a58c231c6bb
Manuel Places Alvarez <mplacesalvarez@danielcastelao.org>
2021-02-09 10:58:42 +0100

    Tercer commit en la rama Colaborador

--- src/com/squash/Colaborador.java
+++ src/com/squash/Colaborador.java
@@ -3,7 +3,7 @@ package com.squash;
 public class Colaborador {
     public void mensajes(){
         System.out.println("Estoy en la rama Colaborador");
-
+        System.out.println("Sigo en la rama Colaborador");
 
     }
 }
