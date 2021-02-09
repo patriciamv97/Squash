7271ee4dcfe9590a3b4b64340c038a4e3023652e
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2021-02-09 11:07:39 +0100

    git merge squash colaborador

--- src/com/squash/Main.java
+++ src/com/squash/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Estoy en la rama colaborador");
+        System.out.println("Sigo en la rama colaborador");
     }
 }
